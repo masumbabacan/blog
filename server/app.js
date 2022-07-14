173ce50e2a7b5e2b9049b129a92cf50c0d84186d
Masum Babacan <mmasumbabacan@gmail.com>
2022-07-14 14:17:43 +0300

    abc

--- server/app.js
+++ server/app.js
@@ -17,11 +17,13 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
     // Pass to next layer of middleware
+    res.header('Content-Type', 'application/json;charset=UTF-8')
+
     next();
 });
 
