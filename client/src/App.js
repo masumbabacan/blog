4e30d3d4d5a3126a23fe64f5799199204ec78040
ahmetgo <64355476+ahmetgo@users.noreply.github.com>
2022-07-14 12:12:14 +0300

    routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import Login from "./pages/Login";
+import Home from "./pages/Home";
+import { Routes, Route, Link, NavLink } from 'react-router-dom';
 
 const App = () => {
   return (
     <>
-         <Login/>
-         
+    <nav>
+      <NavLink to="/">Anasayfa</NavLink>
+      <NavLink to="/login">Giriş Yap</NavLink>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/login" element={<Login />}/>
+    </Routes>
     </>
   );
 }
 
-export default App
+export default App;
