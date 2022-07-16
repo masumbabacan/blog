9a02ea52163b8d0189277dc71cdf3c8f0b45ea3e
Masum Babacan <mmasumbabacan@gmail.com>
2022-07-16 16:52:13 +0300

    user controller file upload null control commet lines

--- server/utils/fileDelete.js
+++ server/utils/fileDelete.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const fileDelete = async (imagePath) => {
+    //check and delete the desired file
     if (fs.existsSync(path.join(__dirname,'../public' + `${imagePath}`))) {
         fs.unlinkSync(path.join(__dirname,'../public' + `${imagePath}`));
     }
