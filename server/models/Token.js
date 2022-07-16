08eadf1dcb7d06917dbed32f91f5160d2514e185
Masum Babacan <mmasumbabacan@gmail.com>
2022-07-16 13:41:22 +0300

    models update

--- server/models/Token.js
+++ server/models/Token.js
@@ -1,10 +1,22 @@
 const mongoose = require('mongoose');
 
 const TokenSchema = new mongoose.Schema({
-    refreshToken: {type : String, required : true},
-    ip: {type : String, required : true},
-    userAgent : {type : String, required : true},
-    isValid : {type : Boolean, default : true},
+    refreshToken: {
+        type : String, 
+        required : true
+    },
+    ip: {
+        type : String, 
+        required : true
+    },
+    userAgent : {
+        type : String, 
+        required : true
+    },
+    isValid : {
+        type : Boolean, 
+        default : true
+    },
     user: {
         type : mongoose.Types.ObjectId,
         ref : 'User',
