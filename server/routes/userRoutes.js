edafc66ebe1951799f44dcd42148abbc066346d4
Masum Babacan <78920463+masumbabacan@users.noreply.github.com>
2022-07-17 20:35:39 +0300

    a

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,20 +4,30 @@ const {authenticateUser,authorizePermissions} = require("../middleware/authentic
 const { getAllUsers,getUser,showCurrentUser,updateUser,updateUserPassword } = 
 require("../controllers/userController");
 
-<<<<<<< HEAD
 router.route("/").get(
     authenticateUser,
     authorizePermissions('admin'),
     getAllUsers,
 );
-=======
->>>>>>> 6683a778c0cb2e5e318da7b5ac8902b7be47ec0f
 
-router.get("/",authenticateUser,authorizePermissions('admin'),getAllUsers); //get all data
-router.get("/showMe",authenticateUser,showCurrentUser); //show current user data
-router.get("/:id",authenticateUser,getUser); //get single data
-router.patch("/updateUserPassword",authenticateUser,updateUserPassword); //update user password
-router.patch("/updateUser",authenticateUser,updateUser); //update user
+router.route("/showMe").get(
+    authenticateUser,
+    showCurrentUser
+);
 
+router.route("/updateUserPassword").patch(
+    authenticateUser,
+    updateUserPassword
+);
+
+router.route("/updateUser").patch(
+    authenticateUser,
+    updateUser
+);
+
+router.route("/:id").get(
+    authenticateUser,
+    getUser
+);
 
 module.exports = router;
